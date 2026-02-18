397a836d0bdfc00332685651ef0230257c5e0a8b
Maicolyovanny666 <maicolreyes2026@gmail.com>
2026-02-18 01:10:03 +0000

    Solucion en java del ejercicio 4.1 de la pagina 157 del libro luis joyanes aguilar

---
+++ semana 3/Condicionales.java
@@ -0,0 +1,25 @@
+package semana 3;
+public class Condicionales {
+    /**
+     * @param args
+     */
+    public static void main (string[] args){
+        Scanner sc = new Scanner(System.in);
+        // Leer dos números y deducir si están en orden creciente. a<= b
+        int a = 0;
+        system.out.printtln("Digite a:"); //Solicitmaos por teclado el valor a
+        a = sc.nexInt();//capturamos por teclado de "a"
+        system.out.printIn();
+        system.outprintInt(s:"Digite b:");  
+        b = sc.nexIn();  
+        if(a<=b){
+           system.out.print(x:"orden creciente);"
+           {else{
+             system.out.printIn(x: "orden creciente");
+             
+           
+
+        }
+         sc.close();
+    }
+}
