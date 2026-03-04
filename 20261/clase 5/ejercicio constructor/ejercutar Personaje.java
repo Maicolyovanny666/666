6647527cf4ae64b0ebb17f1040272b78959bc3ba
Maicolyovanny666 <maicolreyes2026@gmail.com>
2026-03-04 01:35:22 +0000

    ejercutar personaje. java

---
+++ 20261/clase 5/ejercicio constructor/ejercutar Personaje.java
@@ -0,0 +1,11 @@
+public class EjecutarPersonaje {
+    public static void main(String[] args) {
+       //creacion del objeto tipo personaje 
+
+       personaje p1= new personaje(nombre:"superman";
+        p1.mostrarInfo();
+
+       
+
+    }
+}
