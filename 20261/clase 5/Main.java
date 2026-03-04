06c42a5bdd6d17204a27c9c6fc10fb8101598ce5
Maicolyovanny666 <maicolreyes2026@gmail.com>
2026-03-04 01:16:35 +0000

    guia 5 de celulares

---
+++ 20261/clase 5/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    Run | Debug
+    public static void main(String[] args){
+      celular objTelefono1Celular;
+      celular miTelefono = new Celular ();
+        miTelefono.llamar(123);
+        
+
+
+
+    }
+}]
---
+++ 20261/clase 5/celular.java
@@ -0,0 +1,17 @@
+public class celular {
+   //Atributos 
+   string marca;
+   string modelo;
+   int bateria = 100;
+    //Metodos
+    * Firma del metodo
+    * público
+    * void --> no retorna
+    * el método tiene como nombre "llamar"
+    * tiene un parémetro y es de tipo entero
+    * /
+    public void llamar( int numero){
+          system.out.printIn("Llamamdo al "+ numero+ " ..."):
+         this.bateria-= 5;
+        }
+}
